c41d68796278ad12b8fdad1ac21a40f965eea417
Ederson Torresini <etorresini@ifsc.edu.br>
2024-12-12 23:19:53 -0300

    Melhorar cache do SW

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,47 @@
-const cacheName = "Ifsc-Feira De Jogos-1.1";
-const contentToCache = [
-    "Build/FeiraDejogos1.1.loader.js",
-    "Build/FeiraDejogos1.1.framework.js.gz",
-    "Build/FeiraDejogos1.1.data.gz",
-    "Build/FeiraDejogos1.1.wasm.gz",
-    "TemplateData/style.css"
+const cacheName = "feira-v1.1";
 
-];
+const precacheResources = [
+  "Build/FeiraDejogos1.1.loader.js",
+  "Build/FeiraDejogos1.1.framework.js.gz",
+  "Build/FeiraDejogos1.1.data.gz",
+  "Build/FeiraDejogos1.1.wasm.gz",
+  "TemplateData/style.css",
+  "jogos/arcade/0/logo.png",
+  "jogos/arcade/1/logo.png",
+  "jogos/arcade/2/logo.png",
+  "jogos/arcade/3/logo.png",
+  "jogos/hab/logo.png",
+  "jogos/joginho/logo.png",
+  "jogos/ecos-da-floresta/logo.png",
+  "jogos/misterios-do-amor/logo.png",
+  "jogos/tower-of-challenges/logo.png",
+  "jogos/procurando-o-robo/logo.png",
+  "jogos/caso-paranormal/logo.png",
+  "jogos/meowze/logo.png",
+  "jogos/missao-espacial/logo.png",
+  "jogos/island-adventure/logo.png",
+  "jogos/the-last-keepers/logo.png",
+  "jogos/capetalismo/logo.png",
+  "jogos/salsiheero/logo.png",
+  "jogos/rescue-run/logo.png",
+  "jogos/herdeiras-da-magia/logo.png",
+  "jogos/the-fate-of-the-letters/logo.png",
+  "jogos/eldorya/logo.png"
+]
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
-});
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!')
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)))
+})
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!')
+})
 
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    })
+  )
+})
